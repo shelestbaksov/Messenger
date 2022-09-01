8c1e9a349c30fe4cc9ce3c03208ecd46213c98eb
Leisan Latypova <latley90@gmail.com>
2022-09-01 16:14:48 +0300

    Wrap uo getting started

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  Messenger
 //
 //  Created by Leysan Latypova on 01.09.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
